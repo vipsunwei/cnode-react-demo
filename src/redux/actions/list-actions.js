59599a933588294a1263eac295b9adec12af5b20
yaning <yaning@gmail.com>
2020-04-06 00:26:44 +0800

    在HTML文件中添加哀悼黑白页面的样式

--- src/redux/actions/list-actions.js
+++ src/redux/actions/list-actions.js
@@ -1,15 +1,22 @@
-import { UPDATE_LIST, UPDATE_LIST_SUCCESS, UPDATE_LIST_ERROR } from './../action-types/list-types'
+import {
+  UPDATE_LIST,
+  UPDATE_LIST_SUCCESS,
+  UPDATE_LIST_ERROR
+} from './../action-types/list-types'
 import { baseUrl, topics } from './../../api'
 import { fetchGet } from './../../tool/fetch'
 import { compareFn } from './../../tool/sort'
 
-export const updateList = (payload) => ({ type: UPDATE_LIST, payload })
+export const updateList = payload => ({ type: UPDATE_LIST, payload })
 
-export const updateListSuccess = (payload) => ({ type: UPDATE_LIST_SUCCESS, payload })
+export const updateListSuccess = payload => ({
+  type: UPDATE_LIST_SUCCESS,
+  payload
+})
 
-export const updateListError = (payload) => ({ type: UPDATE_LIST_ERROR, payload })
+export const updateListError = payload => ({ type: UPDATE_LIST_ERROR, payload })
 
-export const getTopicList = (tab) => {
+export const getTopicList = tab => {
   return dispatch => {
     let params = {
       tab,
@@ -17,14 +24,24 @@ export const getTopicList = (tab) => {
     }
     let url = baseUrl + topics
     dispatch(updateList({ loading: true }))
-    fetchGet(url, params).then(response => response.json()).then(result => {
-      result.success && dispatch(updateListSuccess({
-        data: result.data.sort(compareFn({ priority: 'top', propertyName: 'create_at' })),
-        loading: false
-      }))
-    }).catch(error => {
-      console.warn(error)
-      dispatch(updateListError({ loading: false }))
-    })
+    fetchGet(url, params)
+      .then(response => response.json())
+      .then(result => {
+        // console.log(result)
+
+        result.success &&
+          dispatch(
+            updateListSuccess({
+              data: result.data.sort(
+                compareFn({ priority: 'top', propertyName: 'create_at' })
+              ),
+              loading: false
+            })
+          )
+      })
+      .catch(error => {
+        console.warn(error)
+        dispatch(updateListError({ loading: false }))
+      })
   }
 }
--- src/view/book/index.js
+++ src/view/book/index.js
@@ -1,7 +1,18 @@
 import { Component } from 'react'
+// import { fetchGet } from '../../tool/fetch'
+
+// const url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5'
 
 class Book extends Component {
-  render () {
+  // componentWillMount() {
+  //   this.getData()
+  // }
+  // async getData() {
+  //   const res = await fetchGet(url, {})
+  //   const data = JSON.parse(res.data)
+  //   console.log(data)
+  // }
+  render() {
     return '教程 '
   }
 }
