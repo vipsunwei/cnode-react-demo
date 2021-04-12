71b6c2932bcc24f1dde247e7967582457c0a2cf6
example <example@gmail.com>
2021-04-12 19:30:07 +0800

    修改项目配置,去掉打包生成的map文件,更换路由为hashRouter模式,打包部署到服务器

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,36 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const path = require("path");
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+
+// 打包配置
+const addCustomize = () => (config) => {
+  if (process.env.NODE_ENV === "production") {
+    // 关闭sourceMap
+    config.devtool = false;
+    // 配置打包后的文件位置
+    config.output.path = path.join(__dirname, "./build/");
+    config.output.publicPath = "/cnode.react/";
+    // // 添加js打包gzip配置
+    // config.plugins.push(
+    //   new CompressionWebpackPlugin({
+    //     test: /\.js$|\.css$/,
+    //     threshold: 1024,
+    //   }),
+    // )
+  }
+  return config;
+};
+
 module.exports = {
   webpack: override(
-    fixBabelImports('import', {
-      libraryName: 'antd',
-      libraryDirectory: 'es',
-      style: true
+    fixBabelImports("import", {
+      libraryName: "antd",
+      libraryDirectory: "es",
+      style: true,
     }),
     addLessLoader({
-      javascriptEnabled: true
+      javascriptEnabled: true,
       // modifyVars: { '@primary-color': '#1DA57A' }
-    })
-  )
-}
+    }),
+    addCustomize()
+  ),
+};
--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { ConfigProvider } from 'antd'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigProvider } from "antd";
 // import zh_CN from 'antd/lib/locale-provider/zh_CN'
-import zhCN from 'antd/es/locale/zh_CN'
-import store from './redux/store/store'
-import './index.css'
+import zhCN from "antd/es/locale/zh_CN";
+import store from "./redux/store/store";
+import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <ConfigProvider locale={zhCN} autoInsertSpaceInButton={false}>
         <App />
       </ConfigProvider>
     </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+  </HashRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
